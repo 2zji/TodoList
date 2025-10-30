458e94aeaba935c77e468867047e5517b7a94b8d
2zji <yunj3747@gmail.com>
2025-10-30 15:10:30 +0900

    user 삭제 기능 추가

--- todo_list/main.py
+++ todo_list/main.py
@@ -67,3 +67,13 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
     db.delete(db_todo)
     db.commit()
     return {"detail": "Todo deleted"}
+
+#user 삭제
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(TodoUser).filter(TodoUser.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(db_user)
+    db.commit()
+    return {"detail": "User deleted"}
