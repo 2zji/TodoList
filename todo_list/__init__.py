80e3769d76de5499704cfaaa31595c331ab9e877
2zji <yunj3747@gmail.com>
2025-11-04 11:19:13 +0900

    .gitignore 파일에 .env 파일 추가 및 __init__.py에 fastAPI status 추가

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+
 from sqlalchemy.orm import Session
 from .db_connect import SessionLocal, get_db, Todo, TodoUser
 from .schemas import TodoCreate, TodoResponse, TodoUpdate, UserCreate, UserResponse
