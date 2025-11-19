77df5dc71bda66341c5fe97f12c7cf4706c374ee
2zji <yunj3747@gmail.com>
2025-11-19 17:15:37 +0900

    menu 링크 삽입 - 각 버튼 클릭 시, 해당 페이지로 이동

--- todo_list_ui/src/App.jsx
+++ todo_list_ui/src/App.jsx
@@ -1,6 +1,10 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Friends from "./pages/friends/Friends";
+import MyTodo from "./pages/myTodo/MyTodo";
+import Dashboard from "./pages/common/Dashboard";
+import Mypage from "./pages/common/Mypage";
+
 // import './App.css'
 
 const styles = {
@@ -10,37 +14,37 @@ const styles = {
     gap: "10px",
     padding: "20px",
     borderRight: "1px solid #ccc",
-    width: "150px", 
+    width: "200px",
     background: "#f9f9f9",
-  },
-  item: {
-    cursor: "pointer",
-    padding: "8px",
-    borderRadius: "6px",
-    transition: "0.2s",
   },
 };
 
 function App() {
+  const navigate = useNavigate();
+  const handleNavigate = useCallback((path) => {
+    // Navigation logic here
+    console.log(`Navigating to ${path}`);
+    navigate(path); 
+  }, []);
+  
   return (
-    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-
+    <div style={{width: "100vw", height: "100vh", display: "flex" }}>
       <div style={{ ...styles.menu }}>
-          <div style={{ margin: "30px", marginTop: "30px"}}>Dashboard</div>
-          <div style={{ margin: "30px"}}>My TODO</div>
-          <div style={{ margin: "30px"}}>Friends</div>
-          <div style={{ margin: "30px"}}>My Page</div>
-          <div style={{ margin: "30px", position: "absolute", bottom: 0}}>Logout</div>
+        <div style={{ margin: "30px", marginTop: "30px" }} onClick={()=>handleNavigate("")}>Dashboard</div>
+        <div style={{ margin: "30px", marginTop: "30px" }} onClick={()=>handleNavigate("myTodo")}>My TODO</div>
+        <div style={{ margin: "30px", marginTop: "30px" }} onClick={()=>handleNavigate("friends")}>Friends</div>
+        <div style={{ margin: "30px", marginTop: "30px" }} onClick={()=>handleNavigate("mypage")}>My Page</div>
+        <div style={{ margin: "30px", position: "absolute", bottom: 0 }} onClick={()=>handleNavigate("logout")}>Logout</div>
       </div>
-
-      <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <div style={{height: "100px",width: "calc(100% - 20px)",fontSize: "30px",display: "flex",alignItems: "center",justifyContent: "flex-start",paddingLeft: "20px"}}>
-          Hi User!
-        </div>
-        <div style={{flex: 1,width: "100%",display: "flex",alignItems: "center",justifyContent: "center"}}>
-          Main
-        </div>
+      <div style={{flex: 1}}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/myTodo" element={<MyTodo />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
       </div>
+    
     </div>
   );
 }
